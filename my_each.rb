ba250ddad3053fb8af149f81e86572fb86d3064a
Whittney Davis <wcdavis89@yahoo.com>
2019-11-13 00:56:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  def hello_t(array)
+  array.each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  else
+    puts "#{name}."
+  end
+  end
+end
 end
